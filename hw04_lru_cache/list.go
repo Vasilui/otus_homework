8a803aa44c8bf288645660dcfc797ef768130bc2
v.kuzminykh <v.kuzminykh@vk.team>
2023-09-19 22:55:22 +0300

    linked list realisation

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,81 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	len   int
+	front *ListItem
+	back  *ListItem
+}
+
+// Len - длина списка.
+func (l *list) Len() int {
+	return l.len
+}
+
+// Front - первый элемент списка.
+func (l *list) Front() *ListItem {
+	return l.front
+}
+
+// Back -последний элемент списка.
+func (l *list) Back() *ListItem {
+	return l.back
+}
+
+// PushFront - добавить значение в начало.
+func (l *list) PushFront(v interface{}) *ListItem {
+	item := &ListItem{Value: v}
+	if l.front == nil {
+		l.front = item
+		l.back = item
+	} else {
+		item.Next, l.front.Prev = l.front, item
+		l.front = item
+	}
+	l.len++
+	return l.front
+}
+
+// PushBack - добавить значение в конец.
+func (l *list) PushBack(v interface{}) *ListItem {
+	item := &ListItem{Value: v}
+	if l.back == nil {
+		l.back = item
+		l.front = item
+	} else {
+		item.Prev, l.back.Next = l.back, item
+		l.back = item
+	}
+	l.len++
+	return l.back
+}
+
+// Remove - удалить элемент.
+func (l *list) Remove(i *ListItem) {
+	if i == l.front {
+		l.front, l.front.Next.Prev = l.front.Next, nil
+	} else if i == l.back {
+		l.back, l.back.Prev.Next = l.back.Prev, nil
+	} else {
+		i.Prev.Next = i.Next
+		i.Next.Prev = i.Prev
+		i.Next = nil
+		i.Prev = nil
+	}
+	l.len--
+}
+
+// MoveToFront - переместить элемент в начало
+func (l *list) MoveToFront(i *ListItem) {
+	if i == l.front {
+		return
+	} else if i == l.back {
+		i.Prev.Next, l.back = nil, i.Prev
+	} else {
+		i.Prev.Next, i.Next.Prev = i.Next, i.Prev
+	}
+
+	i.Next, i.Prev, l.front.Prev = l.front, nil, i
+	l.front = i
 }
 
 func NewList() List {
